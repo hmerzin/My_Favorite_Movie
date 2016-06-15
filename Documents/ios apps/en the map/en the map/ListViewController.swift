19d54048085f3d1d9e19b1f2eee16bf40c449d69
Harry Merzin <harrymerzin@Harrys-MacBook-Air.local>
2016-06-15 13:21:53 -0400

    Initial commit

---
+++ Documents/ios apps/en the map/en the map/ListViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ListViewController.swift
+//  en the map
+//
+//  Created by Harry Merzin on 5/31/16.
+//  Copyright © 2016 harry. All rights reserved.
+//
+
+//import Foundation
+//import UIKit
+//class ListViewController: UIViewController, UITableViewDelegate{
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        /* http://stackoverflow.com/questions/26956728/changing-the-status-bar-color-for-specific-viewcontrollers-using-swift-in-ios8 */
+//        UIApplication.sharedApplication().statusBarStyle = .LightContent
+//        /* http://stackoverflow.com/questions/24687238/changing-navigation-bar-color-in-swift */
+//        navigationController!.navigationBar.barTintColor = UIColor.orangeColor()
+//        /* http://stackoverflow.com/questions/26048765/how-to-set-navigation-bar-font-colour-and-size */
+//        self.navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.whiteColor()]
+//        UINavigationBar.appearance().tintColor = UIColor.whiteColor()
+//    }
+//}
